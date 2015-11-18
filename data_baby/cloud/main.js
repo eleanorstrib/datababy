f6dadd4f548a30e9a7f5141019b40c17cd39357a
eleanorstrib <eleanor@sloan.mit.edu>
2015-11-17 17:24:37 -0800

    successful test creating an object and returning after deploying parse app

--- data_baby/cloud/main.js
+++ data_baby/cloud/main.js
@@ -1,6 +1,19 @@
 
+
+// create subclass of parse object
+var TextData = Parse.Object.extend("TextData");
+
+var textData = new TextData()
+
+textData = {
+	userId: 12345,
+	mobile: 4155555555,
+	message: "bottle",
+	timestamp:"10/23/2015"
+};
+
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  response.success(textData);
 });
